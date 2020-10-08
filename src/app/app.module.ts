1428a4a76b2972086e6c407f9503b56c72840110
hectorfabiopv <hectorfabiopv@gmail.com>
2020-10-08 17:32:44 -0500

    feat: Adición servicio e interfaz de Youtube, se visualizan videos suministrando id del canal #hector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LeftmenuComponent } from './shared/leftmenu/leftmenu.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LeftmenuComponent
+    LeftmenuComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { YoutubeResponse } from '../models/youtube.models'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyDcFA_SGcaT3kNbDzIxKuF493KjwdAabcQ'; 
+  private playlist = 'UU_V38yMrfF8VxFTDgw4IbhA';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) { }
+
+  getVideos() {
+    const url = `${ this.youtubeUrl }/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlist)
+      .set('key', this.apikey)
+      .set('pageToken', this.nextPageToken);
+      return this.http.get<YoutubeResponse>(url, { params })
+        .pipe(map(resp=>{
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }), map( items => items.map( video => video.snippet )))
+  } 
+}
